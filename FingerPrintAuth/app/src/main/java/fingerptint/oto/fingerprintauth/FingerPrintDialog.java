28171c9da3d22a07740f3c1fae51cf578d552c69
oto iantbelidze <oto@fina2.net>
2016-11-28 15:48:15 +0400

    improve ui

--- FingerPrintAuth/app/src/main/java/fingerptint/oto/fingerprintauth/FingerPrintDialog.java
+++ FingerPrintAuth/app/src/main/java/fingerptint/oto/fingerprintauth/FingerPrintDialog.java
@@ -2,12 +2,15 @@ package fingerptint.oto.fingerprintauth;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +26,8 @@ public class FingerPrintDialog {
     private Dialog dialog;
     private ImageView imageView;
     private Activity activity;
+    private TextView messageView;
+    private RelativeLayout root;
 
     public void showDialog(final Activity activity, String msg) {
         this.activity = activity;
@@ -33,6 +38,12 @@ public class FingerPrintDialog {
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setCancelable(false);
         dialog.setContentView(R.layout.finger_print_dialog);
+        root = (RelativeLayout) dialog.findViewById(R.id.linearId);
+        root.setBackgroundColor(Color.TRANSPARENT);
+        RelativeLayout bottom = (RelativeLayout) dialog.findViewById(R.id.bottomLayout);
+        bottom.setBackgroundColor(Color.WHITE);
+
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
 
         FingerPrintUtil util = FingerPrintUtil.getInstance(this, activity);
         util.init();
@@ -41,14 +52,14 @@ public class FingerPrintDialog {
         Window window = dialog.getWindow();
         lp.copyFrom(window.getAttributes());
 
-        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        lp.width = WindowManager.LayoutParams.WRAP_CONTENT;
         lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
         window.setAttributes(lp);
 
-        final TextView text = (TextView) dialog.findViewById(R.id.text_dialog);
+        messageView = (TextView) dialog.findViewById(R.id.text_dialog);
         imageView = (ImageView) dialog.findViewById(R.id.finger_print_image);
 
-        text.setText(msg);
+        messageView.setText(msg);
 
         TextView cancel = (TextView) dialog.findViewById(R.id.tv_cancel);
         cancel.setOnClickListener(new View.OnClickListener() {
@@ -78,8 +89,10 @@ public class FingerPrintDialog {
 
     public void onAuthFailed() {
         Animation shake = AnimationUtils.loadAnimation(activity, R.anim.shake);
-        imageView.startAnimation(shake);
+        root.startAnimation(shake);
         imageView.setImageResource(R.drawable.ic_finger_print_error);
+        messageView.setText("Authentication failed, try again...");
+        messageView.setTextColor(Color.RED);
     }
 
     public void hide() {
